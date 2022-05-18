001afdbc0a8b70a062d1e8cb8a15459beb01d46a
Durgaagrawal <68588058+Durgaagrawal@users.noreply.github.com>
2022-05-18 19:11:34 +0100

    Removed one test class

--- Team/Team/src/test/java/com/TeamService1/Team/TeamApplicationTests.java
+++ Team/Team/src/test/java/com/TeamService1/Team/TeamApplicationTests.java
@@ -1,13 +1,47 @@
 package com.TeamService1.Team;
 
+
+
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest
-class TeamApplicationTests {
+import com.TeamService1.Team.Controller.TeamController;
+import com.TeamService1.Team.Entity.Team;
+import com.TeamService1.Team.Service.TeamService;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = TeamController.class)
+public class TeamApplicationTests {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private TeamService teamService;
+	
+	Team team=new Team(101,"RCB",8000);
+	
+	//String exampleTeamJson=
+	
 	@Test
-	void contextLoads() {
+	public void retrivedetailsforTeam() throws Exception{
+		Mockito.when(teamService.fetchTeamByName("RCB")).thenReturn(team);
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/team/teamByName/RCB").accept(MediaType.APPLICATION_JSON);
+		MvcResult result=mockMvc.perform(requestBuilder).andReturn();
+		System.out.println(result.getResponse());
+		String expected ="{team_id:101,teamName:RCB,teamMaxBudget:8000}";
+		JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(),false);
+		
 	}
-
 }
